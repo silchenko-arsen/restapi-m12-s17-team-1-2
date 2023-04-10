dc4afbb965e7bcf92fe9cc4ce31b0de1d829fa2b
Maria Lazareva <m.lazareva20040625@gmail.com>
2023-04-10 18:05:17 +0300

    Implement TaskController.java

--- src/main/java/com/softserve/itacademy/todolist/controller/TaskController.java
+++ src/main/java/com/softserve/itacademy/todolist/controller/TaskController.java
@@ -1,67 +1,118 @@
 package com.softserve.itacademy.todolist.controller;
-
 import com.softserve.itacademy.todolist.model.Task;
-import com.softserve.itacademy.todolist.model.ToDo;
+import com.softserve.itacademy.todolist.model.User;
 import com.softserve.itacademy.todolist.repository.TaskRepository;
-import com.softserve.itacademy.todolist.repository.ToDoRepository;
+import com.softserve.itacademy.todolist.service.StateService;
+import com.softserve.itacademy.todolist.service.TaskService;
 import com.softserve.itacademy.todolist.service.ToDoService;
-import org.apache.velocity.exception.ResourceNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.softserve.itacademy.todolist.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users/{u_id}/todos/{taskId}/tasks")
+@RequestMapping("/api/")
 public class TaskController {
-    @Autowired
-    private TaskRepository taskRepository;
+    private final TaskService taskService;
+    private final ToDoService todoService;
+    private final StateService stateService;
 
-    @Autowired
-    private ToDoService toDoService;
+    private final TaskRepository taskRepository;
 
-    @GetMapping
-    public List<Task> getAllTasksForTodo(@PathVariable Long taskId) {
-        return taskRepository.getByTodoId(taskId);
+    private final ToDoService toDoService;
+    private final UserService userService;
+
+
+    public TaskController(TaskRepository taskRepository, ToDoService toDoService, TaskService taskService, ToDoService todoService, StateService stateService, UserService userService) {
+        this.taskService = taskService;
+        this.todoService = todoService;
+        this.stateService = stateService;
+        this.taskRepository = taskRepository;
+        this.toDoService = toDoService;
+        this.userService = userService;
     }
 
-    @GetMapping("/{t_id}")
-    public Task getTaskById(@PathVariable Long taskId) {
+
+    @GetMapping()
+    List<Task> getAllTasks(@PathVariable("user_id") long user_id, @PathVariable("todo_id") long todo_id,
+                      Authentication authentication) {
+        if(userService.readById(user_id) == null || todoService.readById(todo_id) == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        User user = (User) authentication.getPrincipal();
+        long idFromAuth = user.getId();
+        if (user_id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+        return todoService.readById(todo_id).getTasks();
+    }
+
+    @GetMapping("tasks/{taskId}")
+    public Task getTaskById(@PathVariable Long taskId, @PathVariable("user_id") long user_id, @PathVariable("todo_id") long todo_id, @Valid @RequestBody Task task,
+                            Authentication authentication) throws Exception {
+        if(userService.readById(user_id) == null || todoService.readById(todo_id) == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        User user = (User) authentication.getPrincipal();
+        long idFromAuth = user.getId();
+        if (user_id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
         return taskRepository.findById(taskId)
-                .orElseThrow(() -> new ResourceNotFoundException("Task not found with id " + taskId));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
-    @PostMapping
+    @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public Task createTaskForTodo(@PathVariable Long taskId, @RequestBody Task task) {
-        List<Task> tasks = toDoService.readById(taskId).getTasks();
+    Task create(@PathVariable("user_id") long user_id, @PathVariable("todo_id") long todo_id, @Valid @RequestBody Task task,
+                Authentication authentication) {
+        if(userService.readById(user_id) == null || todoService.readById(todo_id) == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        User user = (User) authentication.getPrincipal();
+        long idFromAuth = user.getId();
+        if (user_id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+        List<Task> tasks = todoService.readById(todo_id).getTasks();
         tasks.add(task);
-        toDoService.readById(taskId).setTasks(tasks);
+        todoService.readById(todo_id).setTasks(tasks);
         return task;
     }
 
-    @PutMapping("/{t_id}")
+    @PutMapping("tasks/{taskId}")
     @ResponseStatus(HttpStatus.OK)
-    public Task updateTaskById(@PathVariable Long taskId, @RequestBody Task taskRequest) {
+    public Task updateTaskById(@PathVariable Long taskId, @RequestBody Task taskRequest, @PathVariable("user_id") long user_id, @PathVariable("todo_id") long todo_id, @Valid @RequestBody Task task,
+                               Authentication authentication) throws Exception {
+        if(userService.readById(user_id) == null || todoService.readById(todo_id) == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        User user = (User) authentication.getPrincipal();
+        long idFromAuth = user.getId();
+        if (user_id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
         return taskRepository.findById(taskId)
-                .map(task -> {
+                .map(task1 -> {
                     task.setName(taskRequest.getName());
                     task.setPriority(taskRequest.getPriority());
                     task.setState(taskRequest.getState());
                     return taskRepository.save(task);
                 })
-                .orElseThrow(() -> new ResourceNotFoundException("Task not found with id " + taskId));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    @DeleteMapping("/{todoId}")
+    @DeleteMapping("tasks/{taskId}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<?> deleteTaskById(@PathVariable Long taskId) {
+    public ResponseEntity<?> deleteTaskById(@PathVariable Long taskId, @RequestBody Task taskRequest, @PathVariable("user_id") long user_id, @PathVariable("todo_id") long todo_id, @Valid @RequestBody Task task,
+                                            Authentication authentication) throws Exception {
+        if(userService.readById(user_id) == null || todoService.readById(todo_id) == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        User user = (User) authentication.getPrincipal();
+        long idFromAuth = user.getId();
+        if (user_id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
         return taskRepository.findById(taskId)
-                .map(task -> {
+                .map(task1 -> {
                     taskRepository.delete(task);
                     return ResponseEntity.ok().build();
                 })
-                .orElseThrow(() -> new ResourceNotFoundException("Task not found with id " + taskId));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
