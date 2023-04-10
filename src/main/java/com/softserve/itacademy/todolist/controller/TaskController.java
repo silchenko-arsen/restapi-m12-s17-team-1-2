d6cbef7006ccac7832f3ba14fb9d51b76b3bb42e
Maria Lazareva <m.lazareva20040625@gmail.com>
2023-04-10 18:45:34 +0300

    A little changes in TaskController.java

--- src/main/java/com/softserve/itacademy/todolist/controller/TaskController.java
+++ src/main/java/com/softserve/itacademy/todolist/controller/TaskController.java
@@ -2,8 +2,7 @@ package com.softserve.itacademy.todolist.controller;
 import com.softserve.itacademy.todolist.model.Task;
 import com.softserve.itacademy.todolist.model.User;
 import com.softserve.itacademy.todolist.repository.TaskRepository;
-import com.softserve.itacademy.todolist.service.StateService;
-import com.softserve.itacademy.todolist.service.TaskService;
+
 import com.softserve.itacademy.todolist.service.ToDoService;
 import com.softserve.itacademy.todolist.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -18,60 +17,51 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users/{user_id}/todos/{todo_id}/tasks")
 public class TaskController {
-    private final TaskService taskService;
     private final ToDoService todoService;
-    private final StateService stateService;
 
     private final TaskRepository taskRepository;
 
-    private final ToDoService toDoService;
     private final UserService userService;
 
 
-    public TaskController(TaskRepository taskRepository, ToDoService toDoService, TaskService taskService, ToDoService todoService, StateService stateService, UserService userService) {
-        this.taskService = taskService;
+    public TaskController(TaskRepository taskRepository, ToDoService todoService, UserService userService) {
         this.todoService = todoService;
-        this.stateService = stateService;
         this.taskRepository = taskRepository;
-        this.toDoService = toDoService;
         this.userService = userService;
     }
 
 
     @GetMapping()
     List<Task> getAllTasks(@PathVariable("user_id") long user_id, @PathVariable("todo_id") long todo_id,
-                      Authentication authentication) {
+                           Authentication authentication) {
+
+        authorize(user_id, authentication);
         if(userService.readById(user_id) == null || todoService.readById(todo_id) == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        User user = (User) authentication.getPrincipal();
-        long idFromAuth = user.getId();
-        if (user_id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        }
         return todoService.readById(todo_id).getTasks();
     }
 
     @GetMapping("tasks/{taskId}")
-    public Task getTaskById(@PathVariable Long taskId, @PathVariable("user_id") long user_id, @PathVariable("todo_id") long todo_id, @Valid @RequestBody Task task,
-                            Authentication authentication) throws Exception {
+    public Task getTaskById(
+            @PathVariable Long taskId,
+            @PathVariable("user_id") long user_id,
+            @PathVariable("todo_id") long todo_id,
+            Authentication authentication) {
+
+        authorize(user_id, authentication);
         if(userService.readById(user_id) == null || todoService.readById(todo_id) == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        User user = (User) authentication.getPrincipal();
-        long idFromAuth = user.getId();
-        if (user_id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        }
         return taskRepository.findById(taskId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
-    Task create(@PathVariable("user_id") long user_id, @PathVariable("todo_id") long todo_id, @Valid @RequestBody Task task,
-                Authentication authentication) {
+    Task create(
+            @PathVariable("user_id") long user_id,
+            @PathVariable("todo_id") long todo_id,
+            @Valid @RequestBody Task task,
+            Authentication authentication) {
+
+        authorize(user_id, authentication);
         if(userService.readById(user_id) == null || todoService.readById(todo_id) == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        User user = (User) authentication.getPrincipal();
-        long idFromAuth = user.getId();
-        if (user_id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        }
         List<Task> tasks = todoService.readById(todo_id).getTasks();
         tasks.add(task);
         todoService.readById(todo_id).setTasks(tasks);
@@ -80,14 +70,16 @@ public class TaskController {
 
     @PutMapping("tasks/{taskId}")
     @ResponseStatus(HttpStatus.OK)
-    public Task updateTaskById(@PathVariable Long taskId, @RequestBody Task taskRequest, @PathVariable("user_id") long user_id, @PathVariable("todo_id") long todo_id, @Valid @RequestBody Task task,
-                               Authentication authentication) throws Exception {
+    public Task updateTaskById(
+            @PathVariable Long taskId,
+            @RequestBody Task taskRequest,
+            @PathVariable("user_id") long user_id,
+            @PathVariable("todo_id") long todo_id,
+            @Valid @RequestBody Task task,
+            Authentication authentication) {
+
+        authorize(user_id, authentication);
         if(userService.readById(user_id) == null || todoService.readById(todo_id) == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        User user = (User) authentication.getPrincipal();
-        long idFromAuth = user.getId();
-        if (user_id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        }
         return taskRepository.findById(taskId)
                 .map(task1 -> {
                     task.setName(taskRequest.getName());
@@ -100,19 +92,28 @@ public class TaskController {
 
     @DeleteMapping("tasks/{taskId}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<?> deleteTaskById(@PathVariable Long taskId, @RequestBody Task taskRequest, @PathVariable("user_id") long user_id, @PathVariable("todo_id") long todo_id, @Valid @RequestBody Task task,
-                                            Authentication authentication) throws Exception {
+    public ResponseEntity<?> deleteTaskById(
+            @PathVariable Long taskId,
+            @PathVariable("user_id") long user_id,
+            @PathVariable("todo_id") long todo_id,
+            @Valid @RequestBody Task task,
+            Authentication authentication) {
+
+        authorize(user_id, authentication);
         if(userService.readById(user_id) == null || todoService.readById(todo_id) == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        User user = (User) authentication.getPrincipal();
-        long idFromAuth = user.getId();
-        if (user_id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        }
         return taskRepository.findById(taskId)
                 .map(task1 -> {
                     taskRepository.delete(task);
                     return ResponseEntity.ok().build();
                 })
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public void authorize(long id, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        long idFromAuth = user.getId();
+        if (id != idFromAuth && !userService.readById(idFromAuth).getRole().getName().equals("ADMIN")) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
     }
 }
